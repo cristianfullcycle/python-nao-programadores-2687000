059c089fe278d122f1c31f3e3eac0472dfa0254d
cristianfullcycle <cpiccinini@gmail.com>
2025-05-19 12:39:27 +0000

    Teste

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,17 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+name = 'Cristian'
+surname = 'Piccinini'
+about = '''
+  este é um exemplo
+  de variável com
+  mais de uma linha
+'''
 
 # Declare variáveis de cada tipo numérico
-
+age = 40
+salary = 1518.02
 
 # Declare uma variável do tipo booleano
+flag = True
